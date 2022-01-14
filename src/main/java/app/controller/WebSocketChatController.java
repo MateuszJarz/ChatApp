bef202db51a66538e207746baf39e3b5578f630b
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-01-14 11:52:39 +0100

    Initial commit

---
+++ src/main/java/app/controller/WebSocketChatController.java
@@ -0,0 +1,29 @@
+package app.controller;
+
+import app.domian.WebSocketChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class WebSocketChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/chatapp")
+    public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+        return webSocketChatMessage;
+    }
+
+    @MessageMapping("/chat.newUser")
+    @SendTo("/topic/chatapp")
+    public WebSocketChatMessage newUser(@Payload WebSocketChatMessage webSocketChatMessage,
+                                        SimpMessageHeaderAccessor headerAccessor) {
+        headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+        return webSocketChatMessage;
+    }
+
+}
